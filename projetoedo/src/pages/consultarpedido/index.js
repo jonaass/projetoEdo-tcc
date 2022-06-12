104d4bfa93cec252110396e5c387086251945c34
João Paulo <jpplayer08@email.com>
2022-06-12 13:05:10 -0300

    tabela quase pronta

--- projetoedo/src/pages/consultarpedido/index.js
+++ projetoedo/src/pages/consultarpedido/index.js
@@ -1,367 +1,86 @@
 import './index.scss'
 
-import { Link } from 'react-router-dom';
 
 export default function index() {
 
 return(
-
-<main className='pai'>
-
-    <div className='fundo'>
-        <a to='../menu' > <img src="./images/66822.png" className='img-1' /> </a>
-        <h1 className='tex-1'>Consulta de Pedido</h1>
+<main className='pai pagina'>
 
 
-    </div>
+        <header className="header">
+            <h1 className="titulo">Pedidos</h1>
+        </header>
 
+        <div>
+            <input className="barra-pesquisa" type="text" placeholder="Buscar"></input>
+        </div>
 
-    <div className='filtro'>
-        <input type="text" class="fil-1" />
-
-        <img src="./images/filter-free-icon-font.png" className='img-2'/>
-
-
-    </div>
-
-    <div className='tabl'>
-        <table>
-            <tr>
-                <th>Data</th>
-                <th>Nome</th>
-                <th>Horário</th>
-                <th>Veículo</th>
-                <th>Ações</th>
-            </tr>
-
-            <tr>
-                <td>
-                    21/04
-                </td>
-
-                <td>
-                    Jonas
-                </td>
-
-                <td>
-                    11:00
-                </td>
-
-                <td>
-                    Fiat Uno
-                </td>
-                <td>
-                 <img src="../../assets/images/check-free-icon-font (2).png" width="10" height="10" />
-                 <img src="../../assets/images/cross-free-icon-font (1).png"  width="10" height="10" />
-
-                </td>
-
+        <div className="tabela">
+            <table>
                 <tr>
-                    <td>
-                        21/04
-                    </td>
-    
-                    <td>
-                        Jonas
-                    </td>
-    
-                    <td>
-                        11:00
-                    </td>
-    
-                    <td>
-                        Fiat Uno
-                    </td>
-                    <td>
-                     <img src="../../assets/images/check-free-icon-font (2).png" width="10" height="10" />
-                     <img src="../../assets/images/cross-free-icon-font (1).png"  width="10" height="10" />
-    
-                    </td>
-
+                    <th>data</th>
+                    <th>nome</th>
+                    <th>Horário</th>
+                    <th>veiculo</th>
+                    <th>ações</th>
                 </tr>
 
                 <tr>
+                    <td>20/04</td>
+                    <td>Jonas</td>
+                    <td>16h</td>
+                    <td>fiat uno</td>
                     <td>
-                        21/04
+                        <img>"</img>
                     </td>
-    
-                    <td>
-                        Jonas
-                    </td>
-    
-                    <td>
-                        11:00
-                    </td>
-    
-                    <td>
-                        Fiat Uno
-                    </td>
-                    <td>
-                     <img src="../../assets/images/check-free-icon-font (2).png" width="10" height="10" />
-                     <img src="../../assets/images/cross-free-icon-font (1).png"  width="10" height="10" />
-    
-                    </td>
+                </tr>
 
-                    <tr>
-                        <td>
-                            21/04
-                        </td>
-        
-                        <td>
-                            Jonas
-                        </td>
-        
-                        <td>
-                            11:00
-                        </td>
-        
-                        <td>
-                            Fiat Uno
-                        </td>
-                        <td>
-                         <img src="../../assets/images/check-free-icon-font (2).png" width="10" height="10" />
-                         <img src="../../assets/images/cross-free-icon-font (1).png"  width="10" height="10" />
-        
-                        </td>
-    
-                    </tr>
+                <tr>
+                    <td>21/04</td>
+                    <td>João</td>
+                    <td>13h</td>
+                    <td>celta rebaixado</td>
+                    <td></td>
+                </tr>
 
-                    <tr>
-                        <td>
-                            21/04
-                        </td>
-        
-                        <td>
-                            Jonas
-                        </td>
-        
-                        <td>
-                            11:00
-                        </td>
-        
-                        <td>
-                            Fiat Uno
-                        </td>
-                        <td>
-                         <img src="../../assets/images/check-free-icon-font (2).png" width="10" height="10" />
-                         <img src="../../assets/images/cross-free-icon-font (1).png"  width="10" height="10" />
-        
-                        </td>
-    
-                    </tr>
+                <tr>
+                    <td>19/03</td>
+                    <td>Maria</td>
+                    <td>16h</td>
+                    <td>celta</td>
+                    <td></td>
+                </tr>
 
-                    <tr>
-                        <td>
-                            21/04
-                        </td>
-        
-                        <td>
-                            Jonas
-                        </td>
-        
-                        <td>
-                            11:00
-                        </td>
-        
-                        <td>
-                            Fiat Uno
-                        </td>
-                        <td>
-                         <img src="../../assets/images/check-free-icon-font (2).png" width="10" height="10" />
-                         <img src="../../assets/images/cross-free-icon-font (1).png"  width="10" height="10" />
-        
-                        </td>
-    
-                    </tr>
+                <tr>
+                    <td>10/03</td>
+                    <td>Nicolas</td>
+                    <td>16h</td>
+                    <td>um caminhão fodasse</td>
+                    <td></td>
+                </tr>
 
-                    <tr>
-                        <td>
-                            21/04
-                        </td>
-        
-                        <td>
-                            Jonas
-                        </td>
-        
-                        <td>
-                            11:00
-                        </td>
-        
-                        <td>
-                            Fiat Uno
-                        </td>
-                        <td>
-                         <img src="../../assets/images/check-free-icon-font (2).png" width="10" height="10" />
-                         <img src="../../assets/images/cross-free-icon-font (1).png"  width="10" height="10" />
-        
-                        </td>
-    
-                    </tr>
+                <tr>
+                    <td>20/04</td>
+                    <td>Jonas</td>
+                    <td>16h</td>
+                    <td>fiat uno</td>
+                    <td></td>
+                </tr>
 
-                    <tr>
-                        <td>
-                            21/04
-                        </td>
-        
-                        <td>
-                            Jonas
-                        </td>
-        
-                        <td>
-                            11:00
-                        </td>
-        
-                        <td>
-                            Fiat Uno
-                        </td>
-                        <td>
-                         <img src="../../assets/images/check-free-icon-font (2).png" width="10" height="10" />
-                         <img src="../../assets/images/cross-free-icon-font (1).png"  width="10" height="10" />
-        
-                        </td>
-    
-                    </tr>
-
-                    <tr>
-                        <td>
-                            21/04
-                        </td>
-        
-                        <td>
-                            Jonas
-                        </td>
-        
-                        <td>
-                            11:00
-                        </td>
-        
-                        <td>
-                            Fiat Uno
-                        </td>
-                        <td>
-                         <img src="../../assets/images/check-free-icon-font (2).png" width="10" height="10" />
-                         <img src="../../assets/images/cross-free-icon-font (1).png"  width="10" height="10" />
-        
-                        </td>
-    
-                    </tr>
-
-                    <tr>
-                        <td>
-                            21/04
-                        </td>
-        
-                        <td>
-                            Jonas
-                        </td>
-        
-                        <td>
-                            11:00
-                        </td>
-        
-                        <td>
-                            Fiat Uno
-                        </td>
-                        <td>
-                         <img src="../../assets/images/check-free-icon-font (2).png" width="10" height="10" />
-                         <img src="../../assets/images/cross-free-icon-font (1).png"  width="10" height="10" />
-        
-                        </td>
-    
-                    </tr>
-
-                    <tr>
-                        <td>
-                            21/04
-                        </td>
-        
-                        <td>
-                            Jonas
-                        </td>
-        
-                        <td>
-                            11:00
-                        </td>
-        
-                        <td>
-                            Fiat Uno
-                        </td>
-                        <td>
-                         <img src="../../assets/images/check-free-icon-font (2).png" width="10" height="10" />
-                         <img src="../../assets/images/cross-free-icon-font (1).png"  width="10" height="10" />
-        
-                        </td>
-    
-                    </tr>
-
-                    <tr>
-                        <td>
-                            21/04
-                        </td>
-        
-                        <td>
-                            Jonas
-                        </td>
-        
-                        <td>
-                            11:00
-                        </td>
-        
-                        <td>
-                            Fiat Uno
-                        </td>
-                        <td>
-                         <img src="../../assets/images/check-free-icon-font (2).png" width="10" height="10" />
-                         <img src="../../assets/images/cross-free-icon-font (1).png"  width="10" height="10" />
-        
-                        </td>
-    
-                    </tr>
-
-                    <tr>
-                        <td>
-                            21/04
-                        </td>
-        
-                        <td>
-                            Jonas
-                        </td>
-        
-                        <td>
-                            11:00
-                        </td>
-        
-                        <td>
-                            Fiat Uno
-                        </td>
-                        <td>
-                         <img src="../../assets/images/check-free-icon-font (2).png" width="10" height="10" />
-                         <img src="../../assets/images/cross-free-icon-font (1).png"  width="10" height="10" />
-        
-                        </td>
-    
-                    </tr>
-
+                <tr>
+                    <td>20/04</td>
+                    <td>Jonas</td>
+                    <td>16h</td>
+                    <td>fiat uno</td>
+                    <td></td>
                 </tr>
 
 
-            </tr>
-
-        </table>
-
-
-
-    </div>
-
-
-
+            </table>
+        </div>
 
 </main>
    
-    );
 
-
-
-
-
+)
 }
